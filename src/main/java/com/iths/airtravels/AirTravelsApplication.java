def522a045da418824c17ad20e7b2c3bc6220c7e
Anton Johansson <anton.elis@hotmail.com>
2020-12-01 10:43:58 +0100

    First commit

---
+++ src/main/java/com/iths/airtravels/AirTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.iths.airtravels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirTravelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirTravelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iths/airtravels/AirTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iths.airtravels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirTravelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
